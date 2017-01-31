fd94970005a160d233df162f0b0424b72c112044
LouisBluhm <suspiciouspotato@gmail.com>
2017-01-31 11:22:47 +0000

    More random testing with jwt

--- api/web.go
+++ api/web.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gopkg.in/gin-gonic/gin.v1"
+	"github.com/dgrijalva/jwt-go"
 )
 
 func main() {
@@ -15,6 +16,25 @@ func main() {
 				"status": 200,
 			})
 		})
+		api.GET("/oauth/:token", func(c *gin.Context) {
+			
+			key := []byte(c.Param("token"))
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"token": c.Param("token"),
+				"id": 0,
+			})
+			tokenString, err := token.SignedString(key)
+			if err != nil {
+				c.JSON(401, gin.H{
+					"status": 401,
+					"error": "Invalid token",
+				})
+			}
+			c.JSON(200, gin.H{
+				"status": 200,
+				"access-token":  tokenString,
+			})
+		})
 	}
 
 	router.Run(":8931")
@@ -23,13 +43,14 @@ func main() {
 func AuthRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		token := c.Request.Header.Get("Auth-Token")
+		// token := c.Request.Header.Get("Auth-Token")
 
-		if token != "" {
-			c.Next()
-		} else {
-			AuthError(c, 401, "Auth-Token is required for access to API routes")
-		}
+		// if token != "" {
+		// 	c.Next()
+		// } else {
+		// 	AuthError(c, 401, "Auth-Token is required for access to API routes")
+		// }
+		c.Next()
 	}
 }
 
